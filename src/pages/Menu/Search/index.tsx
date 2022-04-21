b29aa64f6f7de6d12d110e94cc79dd245c5c0396
candinho <candinho.junior@supero.com.br>
2022-04-21 10:02:58 -0300

    create the search

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CgSearch } from "react-icons/cg";
+import styles from "./Search.module.scss";
+
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Search({ search, setSearch }: Props) {
+  return (
+    <div className={styles.search}>
+      <input
+        type="search"
+        name="search"
+        id="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <CgSearch size={20} color="#4C4D5E" />
+    </div>
+  );
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,15 +1,23 @@
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Search from "./Search";
+import { useState } from "react";
 
 export default function Menu() {
+  const [search, setSearch] = useState("");
+
   return (
     <main>
-      <nav className={styles.menu}>
+      <nav className={styles.navBar}>
         <Logo />
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>La casa do código e da massa</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Cardápio</h3>
+        <Search search={search} setSearch={setSearch} />
+      </section>
     </main>
   );
 }

75cd9c465c589b2c4ea80eae0e3a93612c6017bb
candinho <candinho.junior@supero.com.br>
2022-04-21 12:22:44 -0300

    create the filters

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,10 +1,13 @@
-import styles from "./Menu.module.scss";
+import { useState } from "react";
+
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Search from "./Search";
-import { useState } from "react";
+import Filters from "./Filters";
+import styles from "./Menu.module.scss";
 
 export default function Menu() {
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<number | null>(null);
 
   return (
     <main>
@@ -12,11 +15,14 @@ export default function Menu() {
         <Logo />
       </nav>
       <header className={styles.header}>
-        <div className={styles.header__text}>La casa do código e da massa</div>
+        <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
       <section className={styles.menu}>
         <h3 className={styles.menu__title}>Cardápio</h3>
         <Search search={search} setSearch={setSearch} />
+        <div className={styles.menu__filters}>
+          <Filters filter={filter} setFilter={setFilter} />
+        </div>
       </section>
     </main>
   );
